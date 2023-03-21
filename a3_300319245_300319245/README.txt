bf8dfd236da44aac1c4722d9f371bfb4c58c4553
jvdlopes <jvdlopes04@gmail.com>
2023-03-21 01:29:23 -0400

    Assignment 3 part 2

---
+++ a3_300319245_300319245/README.txt
@@ -0,0 +1,8 @@
+Student name: John Lopes
+Student number: 300319245
+Course code: ITI1121
+Lab section: A-03
+
+This archive contains the 14 files of assignment 3, that is, this file README.txt,
+plus CapacityOptimizer.java, Car.java, LinkedQueue.java, List.java, ParkingLot.java, 
+Queue.java, RandomGenerator.java, Rational.java, Simulator.java, SinglyLinkedList.java, Spot.java, StudentInfo.java and TriangularDistribution.java.
---
+++ a3_300319245_300319245/StudentInfo.java
@@ -0,0 +1,28 @@
+/**
+* Contains a method <code>void display()</code> that all the <code>main</code>
+* methods call to show the student information. Fill the box with your personal
+* information.
+*
+* @author Marcel Turcotte (marcel.turcotte@uottawa.ca)
+*/
+
+public class StudentInfo {
+
+    /**
+    * Displays the student information: student name, id, section, etc for each
+    * member of the team.
+    */
+
+    public static void display() {
+
+        System.out.println("************************************************************");
+        System.out.println("*John Lopes                                                *");
+        System.out.println("*300319245                                                 *");
+        System.out.println("*Course code: ITI1121                                      *");
+        System.out.println("*Lab Section: A-03                                         *");
+        System.out.println("************************************************************");
+        System.out.println();
+
+    }
+
+}
